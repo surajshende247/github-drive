28e11427f79e6f5160f7c2382eec570dd6457837
Suraj Shende <surajshende247@gmail.com>
2022-10-25 05:59:57 +0530

    feat: octocat client

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import { Octokit } from "@octokit/core";
 import axios from 'axios';
 dotenv.config()
+const octokit = new Octokit({ auth: `${process.env.GITHUB_TOKEN}` });
 
 const PORT = process.env.PORT || 5000;
-
-const BASE_URL = "https://api.github.com/repos"
 
 const app = express();
 app.use(express.json({ limit: '50mb' }));
@@ -18,24 +18,39 @@ mongoose.connect(process.env.MONGODB_URI, () => {
 app.post('/upload', async (req, res) => {
   const { fileName, content } = req.body;
   const timestamp = Date.now();
-  const githubAPIResponse = await axios.put(`${BASE_URL}/rtcmay4/s3/contents/${timestamp}${fileName}`, {
-    "owner": "OWNER",
-    "repo": "REPO",
-    "path": "PATH",
-    "message": "file created using API call",
-    "committer": {
-      "name": "Suraj Shende",
-      "email": "surajshende247@gmail.com"
+  const githubAPIResponse = await octokit.request(`PUT /repos/rtcmay4/s3/contents/${timestamp}${fileName}`, {
+    owner: 'OWNER',
+    repo: 'REPO',
+    path: 'PATH',
+    message: 'my commit message',
+    committer: {
+      name: 'Monalisa Octocat',
+      email: 'octocat@github.com'
     },
-    "content": content
-  }, {
-    headers: {
-      "Accept": "application/vnd.github.v3+json",
-      "Authorization": `Bearer ${process.env.GITHUB_TOKEN}`
-    }
+    content: content
   })
-  console.log(content);
   res.send(githubAPIResponse.data);
+})
+
+app.post('/delete', async (req, res) => {
+  const {fileName, sha} = req.body;
+  const githubAPIResponse = await octokit.request(`DELETE /repos/rtcmay4/s3/contents/${fileName}`, {
+    owner: 'OWNER',
+    repo: 'REPO',
+    path: 'PATH',
+    message: 'my commit message',
+    committer: {
+      name: 'Monalisa Octocat',
+      email: 'octocat@github.com'
+    },
+    sha: sha
+  })
+  res.send(githubAPIResponse.data)
+})
+
+app.post('/contents', async(req, res) => {
+  const githubAPIResponse = await octokit.request(`GET /repos/rtcmay4/s3/contents/`)
+  res.send(githubAPIResponse.data)
 })
 
 
