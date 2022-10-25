31f7e1057b36467a8830173e4419e126d27760b8
Suraj Shende <surajshende247@gmail.com>
2022-10-25 06:00:21 +0530

    basic file explorer

--- client/src/views/Explorer/Explorer.js
+++ client/src/views/Explorer/Explorer.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from "axios"
 
 import "./Explorer.css"
 
 function Explorer() {
+  const [files, setFiles] = useState([]);
+  useEffect(()=>{
+    async function getData(){
+      const response = await axios.post('/contents')
+      setFiles(response.data)
+      console.log(response.data)
+    }
+    getData()
+  }, [])
   return (
-    <div>Explorer</div>
+    <div>
+      <h1>Explorer</h1>
+      {
+        files.map((file, index) => {
+          return (
+            <div key={index}>
+              <img src={file.download_url}  style={{height: "100px"}}/>
+            </div>
+          )
+        })
+      }
+    </div>
   )
 }
 
